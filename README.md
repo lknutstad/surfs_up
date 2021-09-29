101997d44a7992f6fda5dbc323e55878be46f6f8
lknutstad <lknutstad@gmail.com>
2021-09-29 02:57:35 +0200

    challenge ready for submisson

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # surfs_up
+
+## Overview
+
+### Purpose
+
+While trying to open a surf shop, a friend has reached out for help with a weather data set to see how it may impact his business
+
+## Analysis
+- June and December had similar average temperatures (within a few degrees).
+- More temperature measurements were taken during June than in December
+- The maximum temps were higher in August than in December
+- The low temps were lower in December than in August
+
+## Summary
+- I would include precipitation, as this effects surf conditions as much as temperature
+- I would include wind, as this effects surf conditions as much as temperature
--- app.py
+++ app.py
@@ -1,38 +1,34 @@
-# Dependencies
+# Import dependencies
 import datetime as dt
 import numpy as np
 import pandas as pd
-
-# Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-# Import flask dependencies
 from flask import Flask, jsonify
 
-# Set up database engine
+# Create engine
 engine = create_engine("sqlite:///hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect database into a new model
 Base = automap_base()
 
-# reflect the tables
+# Reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save references to each table
+# Save the references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-# Set up flask
+# Define Flask app
 app = Flask(__name__)
 
-# Define routes
-@app.route("/")
+# Create the Welcome route
+@app.route('/')
 def welcome():
     return(
     '''
@@ -44,10 +40,47 @@ def welcome():
     /api/v1.0/temp/start/end
     ''')
 
+# Create Precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-   precipitation = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= prev_year).all()
-   precip = {date: prcp for date, prcp in precipitation}
-   return jsonify(precip)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+# Create Stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# Create Monthly Temperature route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# Create a Statistics route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
